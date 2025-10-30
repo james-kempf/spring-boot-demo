c994ca2d5467abe0e137e7df987594d8361fbc30
James Kempf <jamesk4321@gmail.com>
2025-10-30 12:53:39 -0500

    add repository

---
+++ src/main/java/com/example/demo/repository/PersonRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Person;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends MongoRepository<Person, String> {
+
+    Person findByLastName(@Param("name") String name);
+
+}
