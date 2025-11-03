02649c83d72f27eb2a40f9304e23bfb16062b285
James Kempf <jamesk4321@gmail.com>
2025-11-03 14:02:10 -0600

    add tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.demo;
 
+import com.example.demo.controller.DemoController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    DemoController demoController;
+
+    @Test
+    void contextLoads() throws Exception {
+        assertNotNull(demoController);
+    }
+
+    @Test
+    void helloShouldReturnDefaultMessage() throws Exception {
+        String response = restTemplate.getForObject("http://localhost:" + port + "/hello", String.class);
+        assertEquals("Hello World!", response);
+    }
+
+    @Test
+    void helloShouldReturnGivenMessage() {
+        String response = demoController.hello("test");
+        assertEquals("Hello test!", response);
+    }
 
 }
