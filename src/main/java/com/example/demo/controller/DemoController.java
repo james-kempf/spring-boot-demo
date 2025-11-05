05ec9af2e16989dfcf442ae85d1a3ef1743e1f22
James Kempf <jamesk4321@gmail.com>
2025-11-05 12:11:34 -0600

    forward http response

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,11 +3,19 @@ package com.example.demo.controller;
 import com.example.demo.model.Greeting;
 import com.example.demo.service.KafkaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
@@ -28,6 +36,22 @@ public class DemoController {
         var i = 0;
         System.out.println(i);
         return new Greeting(counter.getAndIncrement(), "Hello " + name);
+    }
+
+    @GetMapping("/http-client")
+    public ResponseEntity<String> httpClient() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/todos/1"))
+                .GET().build();
+
+        try {
+            String body = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+            return ResponseEntity.ok(body);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("Error");
+        }
     }
 
     @PostMapping("/publish")
